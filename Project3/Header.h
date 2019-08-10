3595178c4deb7e702a870c09b489651cdd93be4c
Uzair Ahmad Khan <uzairahmadkhan1993@gmail.com>
2019-08-10 04:21:04 +0200

    hello

---
+++ Project3/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+void main1(int x) {
+
+
+	cout << "Coming from header file";
+	cout << endl<<"The sum is " << endl << (x + x);
+}
---
+++ Project3/Source.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+using namespace std;
+
+void main() {
+	int x;
+	cout << endl << "Enter the number " << endl;
+	cin >> x;
+	main1(x);
+
+
+
+}
